1074d49af2758292ae419f9819ac991ea322cc19
Pablo <pawel.stolka@gmail.com>
2017-07-07 18:06:14 +0200

    Autofac works and dispatchers under construction

---
+++ FollowMe.Infrastructure/Commands/CommandDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace FollowMe.Infrastructure.Commands
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private readonly IComponentContext _context;
+
+        public CommandDispatcher(IComponentContext context)
+        {
+            _context = context;
+        }
+        public async Task DispatchAsync<T>(T command) where T : ICommand
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FollowMe.Infrastructure/Handlers/Categories/CreateCategoryHandler.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using FollowMe.Infrastructure.Commands;
+using FollowMe.Infrastructure.Commands.Category;
+using FollowMe.Infrastructure.Repositories;
+using FollowMe.Infrastructure.Services;
+using System.Web.Http;
+
+namespace FollowMe.Infrastructure.Handlers.Categories
+{
+    public class CreateCategoryHandler : ICommandHandler<CreateCategory>
+    {
+        private ICategoryService _categoryService;
+
+        public CreateCategoryHandler()
+        {
+            var _categoryRepo = new InMemoryCategoryRepo();
+            _categoryService = new CategoryService(_categoryRepo);
+
+        }
+
+        public async Task HandleAsync(CreateCategory command)
+        {
+            await _categoryService
+                .RegisterAsync(command.Name, command.Description);
+        }
+    }
+}
